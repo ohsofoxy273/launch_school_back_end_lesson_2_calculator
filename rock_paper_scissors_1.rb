247281fae74583ea3e3e91fb4f7180fe91005ecc
ohsofoxy273 <matthew.f.fox@gmail.com>
2016-09-22 19:28:57 -0500

    first rack paper scissors

---
+++ rock_paper_scissors_1.rb
@@ -0,0 +1,49 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def display_results(player, computer)
+    if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+
+     prompt("You won!")
+
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+
+        prompt("You lost!")
+  else
+    prompt("It's a tie!")
+  end
+
+end
+
+loop do
+
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice.")
+    end
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  puts "You chose: #{choice}; computer chose: #{computer_choice}"
+
+  display_results(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+
+  break unless answer.downcase.start_with?('y')
+end
